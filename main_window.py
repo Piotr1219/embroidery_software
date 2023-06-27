90688357e1d7508976151aa41014d05e1bdfcfa8
Wasyl <wbadyra@gmail.com>
2023-06-27 13:20:22 +0200

    Powinno smigac

--- main_window.py
+++ main_window.py
@@ -1,7 +1,10 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QSlider
+
 from main_design_ui import Ui_MainWindow
 from image_processing import *
+import numpy as np
 
 
 class MainWindowDesign(Ui_MainWindow):
@@ -9,6 +12,8 @@ class MainWindowDesign(Ui_MainWindow):
         super().__init__()
         self.image = None
         self.image_original = None
+        self.temp_image = None
+        self.last_operation = 0
 
     def add_actions(self):
         self.scene = QtWidgets.QGraphicsScene(self)
@@ -19,6 +24,13 @@ class MainWindowDesign(Ui_MainWindow):
         self.pushButton_3.clicked.connect(self.greyscale)
         self.pushButton_4.clicked.connect(self.reset)
         self.pushButton_2.clicked.connect(self.start_machine)
+        self.pushButton_color.clicked.connect(lambda: self.reduce_colors())
+        self.pushButton_background.clicked.connect(self.remove_background)
+        self.pushButton_sketch.clicked.connect(lambda: self.to_sketch())
+        self.horizontalSlider.valueChanged.connect(lambda: self.update_label(self.horizontalSlider.value()))
+        self.horizontalSlider_2.valueChanged.connect(lambda: self.update_label_2(self.horizontalSlider_2.value()))
+        self.horizontalSlider.sliderReleased.connect(lambda: self.to_sketch(self.horizontalSlider.value()))
+        self.horizontalSlider_2.sliderReleased.connect(lambda: self.reduce_colors(self.horizontalSlider_2.value()))
 
     def getfiles(self):
         dlg = QtWidgets.QFileDialog()
@@ -54,12 +66,100 @@ class MainWindowDesign(Ui_MainWindow):
         self.pixmap_item = self.scene.addPixmap(pixMap)
 
     def greyscale(self):
+        self.last_operation = 1
         self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
         self.image = cv2.cvtColor(self.image, cv2.COLOR_GRAY2BGR)
         self.update_image()
 
     def reset(self):
+        self.last_operation = 0
         self.image = self.image_original
+        self.label.setVisible(False)
+        self.horizontalSlider.setVisible(False)
+        self.temp_image = None
+        self.update_image()
+
+    def reduce_colors(self, k=6):
+        if self.last_operation != 3:
+            self.temp_image = self.image
+        self.last_operation = 3
+        self.label.setVisible(False)
+        self.horizontalSlider.setVisible(False)
+        if self.temp_image is None:
+            self.image = self.image_original
+        else:
+            self.image = self.temp_image
+        self.horizontalSlider_2.setVisible(True)
+        self.horizontalSlider_2.setMinimum(3)
+        self.horizontalSlider_2.setMaximum(15)
+        self.horizontalSlider_2.setValue(k)
+        self.horizontalSlider.setUpdatesEnabled(True)
+        self.label_2.setVisible(True)
+        self.label_2.setText(f"{self.horizontalSlider_2.value()}")
+        self.label.update()
+        self.horizontalSlider.update()
+        pixel_vals = self.image.reshape((-1, 3))
+        pixel_vals = np.float32(pixel_vals)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.85)
+        print("Start")
+        retval, labels, centers = cv2.kmeans(pixel_vals, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        print("Koniec")
+
+        # convert data into 8-bit values
+        centers = np.uint8(centers)
+        segmented_data = centers[labels.flatten()]
+
+        # reshape data into the original image dimensions
+        self.image = segmented_data.reshape((self.image.shape))
+        self.update_image()
+
+    def to_sketch(self, size=21):
+        if self.last_operation != 4:
+            self.temp_image = self.image
+        self.last_operation = 4
+        self.label_2.setVisible(False)
+        self.horizontalSlider_2.setVisible(False)
+        if self.temp_image is None:
+            self.image = self.image_original
+        else:
+            self.image = self.temp_image
+        self.horizontalSlider.setVisible(True)
+        self.horizontalSlider.setMinimum(3)
+        self.horizontalSlider.setMaximum(55)
+        self.horizontalSlider.setValue(size)
+        self.horizontalSlider.setTickPosition(QSlider.TicksBothSides)
+        self.horizontalSlider.setTickInterval(2)
+        self.horizontalSlider.setSingleStep(2)
+        self.horizontalSlider.setUpdatesEnabled(True)
+        self.label.setVisible(True)
+        self.label.setText(f"{self.horizontalSlider.value()}")
+        self.label.update()
+        self.horizontalSlider.update()
+        grey_img = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        invert = cv2.bitwise_not(grey_img)
+        if size % 2 == 0:
+            self.horizontalSlider.setValue(size-1)
+            size = size-1
+        blur = cv2.GaussianBlur(invert, (size, size), 0)
+        invertedblur = cv2.bitwise_not(blur)
+        sketch = cv2.divide(grey_img, invertedblur, scale=256.0)
+        cv2.imwrite("./sketch.png", sketch)  # converted image is saved as mentioned name
+        self.image = cv2.imread("./sketch.png")
+        self.update_image()
+
+    def update_label(self, val):
+        self.label.setText(f"{val}")
+
+    def update_label_2(self, val):
+        self.label_2.setText(f"{val}")
+
+    def remove_background(self):
+        self.last_operation = 2
+        from rembg import remove
+        # self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        self.image = remove(self.image)
+        cv2.imwrite("./test.png", self.image)
+        self.image = cv2.imread("./test.png")
         self.update_image()
 
     def start_machine(self):
